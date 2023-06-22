9e901b4a0c1594005507e50e9b0a6ab952146ea7
Gianluca Nativo <giannativo@yahoo.com.ar>
2023-06-22 16:38:35 -0300

    Fix end of files, removing typeorm config

--- src/cards/cards.module.ts
+++ src/cards/cards.module.ts
@@ -1,15 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CardsController } from './cards.controller';
 import { CardsService } from './cards.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { BugCard } from './entities/bug.card.entity';
-import { IssueCard } from './entities/issue.card.entity';
-import { TaskCard } from './entities/task.card.entity';
 import { PrismaService } from 'src/prisma.service';
 
 @Module({
   controllers: [CardsController],
   providers: [CardsService, PrismaService],
-  /*imports: [TypeOrmModule.forFeature([BugCard, IssueCard, TaskCard])],*/
 })
 export class CardsModule {}
